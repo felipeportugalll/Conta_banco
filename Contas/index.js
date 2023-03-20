ca5e3d9c7d6af79578c75ce0170e8e5d21c3c8cd
felipeportugalll <felipetpramirez@gmail.com>
2023-03-20 17:38:01 -0300

    Criação de conta add

--- Contas/index.js
+++ Contas/index.js
@@ -7,6 +7,7 @@ import fs from 'fs';
 
 operation() //chama a função
 
+//User options
 function operation() {
 
     inquirer.prompt([
@@ -26,7 +27,55 @@ function operation() {
     .then((answer) => {
         const action = answer['action']
         
-        console.log(action)
+        if(action === 'Criar Conta') {
+            createAccount()
+        }
     })
     .catch(err => console.log(err)) 
+}
+
+//Starts the creation of the user account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Obrigado por escolher nosso banco!'))
+    console.log(chalk.green('Por favor, defina as opções da sua conta a seguir.'))
+
+    buildAccount()
+}
+
+//Build the account
+function buildAccount() {
+
+    inquirer
+    .prompt([
+        {
+            name: 'accountName',
+            message: 'Digite seu nome: '
+        }
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) { 
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`,
+         '{"balance": 0}',
+         function(err) {
+            console.log(err)
+         }
+        )
+
+        console.log(chalk.green('Conta criada com sucesso!'))
+        operation()
+    })
+    .catch((err) => console.log(err))
 }
